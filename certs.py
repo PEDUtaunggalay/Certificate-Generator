a39995aba87a276e7981750b7432bd8b2c13d5a7
sawthinkar <10482589+sawthinkar@users.noreply.github.com>
2022-05-17 20:34:18 +0630

    refactor codes, change id from 1001 to 0001

--- certs.py
+++ certs.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 import pandas as pd
 
 #Import the file that contains all the details
-data = pd.read_excel("fb.xlsx")
+data = pd.read_excel("pbm_2022_b1.xlsx")
 
 #Import 'Name' List from the imported .xlsx file
 name_list = data['Name'].to_list()
@@ -14,7 +14,7 @@ name_list = data['Name'].to_list()
 father_list = data['Father'].to_list()
 
 #Import 'Phlon No.' List from the imported .xlsx file
-id_list = data['Phlon No.'].to_list()
+phlon_id_list = data['Phlon No.'].to_list()
 
 #Import 'Gender' List from the imported .xlsx file
 sex_list = data['Sex'].to_list()
@@ -34,17 +34,24 @@ for idx, i in enumerate(name_list):
     # new_img = Image.blend(background, overlay, 0.5)
 
     # getting student id from id_list which is imported from .xlsx file
-    id = id_list[idx]
-    print(id)
-    fm = sex_list[idx]
-    print(fm)
+    phlon_id = phlon_id_list[idx]
+
+    # “how to get a zero before 1-9 in python”
+    # https://www.codegrepper.com/code-examples/python/how+to+get+a+zero+before+1-9+in+python
+    str(phlon_id).zfill(4)
+    print(str(phlon_id).zfill(4))
+    id = str(phlon_id).zfill(4)
     
     # https://www.geeksforgeeks.org/overlay-an-image-on-another-image-in-python/
+    
     # opening certificate template
-    img1 = Image.open(r"pbm-2022-template.jpg") # certa4.jpg
+    img1 = Image.open(r"images/pbm-2022-template.jpg") # certa4.jpg
     # img1.show()
+    
     # opening student's photo 
-    img2 = Image.open("images/photos/pbm-2022/%d.png" % (id))
+    img2 = Image.open("images/pbm-2022/photos/%s.png" % (id))
+
+    # adding student's photo
     # No transparency mask specified, 
     # simulating an raster overlay
     img1.paste(img2, (2760,815))
@@ -57,51 +64,52 @@ for idx, i in enumerate(name_list):
 
     # getting student's gender
     sex = sex_list[idx]
+    print(sex)
 
     # im = Image.open("certa4.jpg")
     # getting ready to start writing names on certificate
     d = ImageDraw.Draw(img1)
-    # location for student name 
-    location = (350, 925)
 
-    # location2 for gender identity 
-    location2 = (293, 1175)
+    # location for student name 
+    location_student_name = (350, 925)
+
+    # location2 for daughter of/son of
+    location_daughter_son = (293, 1175)
 
     # location for father's name 
-    location3 = (300,1300)
+    location_father_name = (300,1300)
 
     # location for Phlon No. 
-    location4 = (2875,1420)
+    location_p_no = (2875,1420)
 
     # blue 
     # text_color = (0, 137, 209)
     text_color = (0, 0, 0)
-    text_color2 = (9,35,138)
+    text_color_daughter_son = (9,35,138)
 
     # orignal font fonts/AlexBrush-Regular.ttf 
-    font = ImageFont.truetype("fonts/SnellRoundhand/SnellBT-Bold.otf", 150, encoding="unic")
+    font_name = ImageFont.truetype("fonts/SnellRoundhand/SnellBT-Bold.otf", 150, encoding="unic")
     # font2 = ImageFont.truetype("fonts/AlexBrush-Regular.ttf", 250, encoding="unic")
-    font2 = ImageFont.truetype("fonts/AvenirNext/AvenirNextLTPro-Regular.otf", 70, encoding="unic")
-
-    font3 = ImageFont.truetype("fonts/AvenirNext/AvenirNextLTPro-Bold.otf", 60, encoding="unic")
+    font_daughter_son = ImageFont.truetype("fonts/AvenirNext/AvenirNextLTPro-Regular.otf", 70, encoding="unic")
+    font_p_no = ImageFont.truetype("fonts/AvenirNext/AvenirNextLTPro-Bold.otf", 60, encoding="unic")
 
     # draw student's name
-    d.text(location, i.title(), fill=text_color, font=font)
+    d.text(location_student_name, i.title(), fill=text_color, font=font_name)
 
     # draw female/male 
     if sex.title() == "Male":
-        d.text(location2, "Son of", fill=text_color2, font=font2)
+        d.text(location_daughter_son, "Son of", fill=text_color_daughter_son, font=font_daughter_son)
     else: 
-        d.text(location2, "Daughter of", fill=text_color2, font=font2)
+        d.text(location_daughter_son, "Daughter of", fill=text_color_daughter_son, font=font_daughter_son)
     
-    # father's name 
-    d.text(location3, father.title(), fill=text_color, font=font)
+    # draw father's name 
+    d.text(location_father_name, father.title(), fill=text_color, font=font_name)
 
-    # draw Phlon No. 
-    d.text(location4, "P-No.%d" % id, fill=text_color, font=font3)
+    # draw Phlon No. under student photo
+    d.text(location_p_no, "P-No.%s" % id, fill=text_color, font=font_p_no)
 
     # img1.show()
-    img1.save("pbm_2022_b1_%d.jpg" % (id))
+    img1.save("images/pbm-2022/certificates/pbm_2022_b1_%s.jpg" % (id))
     print("(%d/%d) Certificate Created for:  %s" % (idx+1, max_no, i.title()))
     
     
